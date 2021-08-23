445203a3cbda993d9f55c460df0027dcd548faf9
Ardyn Sulaeman <lemanfoetra@gmail.com>
2021-08-23 16:28:00 +0700

    init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class UserController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password', 'role');
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        $user = JWTAuth::user();
+        return response()->json(compact('user', 'token'));
+    }
+
+
+    public function register(Request $request)
+    {
+        if ($this->uniqueUser($request->get('email'), $request->get('role')) != null) {
+            return response()->json(['error' => 'Email telah digunakan'], 400);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required|string|max:255',
+            'password'  => 'required|string|min:6|confirmed',
+            'role'      => 'required|string',
+            'email'     => 'required|string|email|max:255|'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'name'      => $request->get('name'),
+            'email'     => $request->get('email'),
+            'role'      => $request->get('role'),
+            'password'  => Hash::make($request->get('password')),
+        ]);
+
+        $token  = JWTAuth::fromUser($user);
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getMessage());
+        } catch (TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getMessage());
+        } catch (JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getMessage());
+        }
+        return response()->json(compact('user'));
+    }
+
+
+    protected function uniqueUser($email, $role)
+    {
+        $user = new User();
+        return $user->select(['email'])
+            ->where('email', $email)
+            ->where('role', $role)
+            ->first();
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('role')->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->string('hashmap_code')->nullable();
+            $table->string('full_address')->nullable();
+            $table->string('provinsi')->nullable();
+            $table->string('kabkot')->nullable();
+            $table->string('kecamatan')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
