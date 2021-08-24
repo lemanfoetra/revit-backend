60c2d278e894762ee7246e83ddc76f596913e52c
Ardyn Sulaeman <lemanfoetra@gmail.com>
2021-08-24 16:06:23 +0700

    update

---
+++ app/Http/Controllers/Seeker/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Seeker;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Home extends Controller
+{
+
+    public function slider()
+    {
+    }
+
+
+    public function bengkelTerdekat($latitude, $longitude)
+    {
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Owner\Service;
+use App\Http\Controllers\Seeker\Home;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -33,10 +34,18 @@ Route::put(
 )->middleware('auth.api.role:SEEKER,OWNER');
 
 
-Route::prefix('service')->group(function () {
-    Route::get('/', [Service::class, 'index'])->middleware('auth.api.role:SEEKER,OWNER');
-    Route::get('/{service_id}', [Service::class, 'show'])->middleware('auth.api.role:OWNER,SEEKER');
-    Route::post('/', [Service::class, 'store'])->middleware('auth.api.role:OWNER');
-    Route::put('/{service}', [Service::class, 'update'])->middleware('auth.api.role:OWNER');
-    Route::delete('/{service}', [Service::class, 'delete'])->middleware('auth.api.role:OWNER');
+Route::prefix('owner')->group(function () {
+    // Route Managment Service
+    Route::get('/service', [Service::class, 'index'])->middleware('auth.api.role:SEEKER,OWNER');
+    Route::get('/service/{service_id}', [Service::class, 'show'])->middleware('auth.api.role:OWNER,SEEKER');
+    Route::post('/service', [Service::class, 'store'])->middleware('auth.api.role:OWNER');
+    Route::put('/service/{service}', [Service::class, 'update'])->middleware('auth.api.role:OWNER');
+    Route::delete('/service/{service}', [Service::class, 'delete'])->middleware('auth.api.role:OWNER');
+});
+
+
+Route::prefix('seeker')->group(function () {
+    // Route Tab Home Seeker
+    Route::get('/slider', [Home::class, 'slider'])->middleware('auth.api.role:SEEKER');
+    Route::get('/bengkel_terdekat/{latitude}/{longitude}', [Home::class, 'bengkelTerdekat'])->middleware('auth.api.role:SEEKER');
 });
